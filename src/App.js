3b155d942862432f6ed57046e0c419c0f8ba2b86
Kasper Pedersen <kasper.dharma@gmail.com>
2022-04-28 20:28:11 +0200

    feat:preview build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import ApplicationFrame from "./components/Frame/ApplicationFrame";
 import LygtenGallery from "./components/Galleries/LygtenGallery";
@@ -8,11 +8,11 @@ import LygtenGallery from "./components/Galleries/LygtenGallery";
 function App() {
   return (
   <div>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path={"/preview"} element={<ApplicationFrame></ApplicationFrame>}></Route>
-        <Route exact path={"/preview/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
-      </Routes>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+        <Routes>
+          <Route exact path={""} element={<ApplicationFrame></ApplicationFrame>}></Route>
+          <Route exact path={"/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
+        </Routes>
     </BrowserRouter>
   </div>
   );
