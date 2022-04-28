010c9e019e45008721f718aaa4c11c7403a17e28
Kasper Pedersen <kasper.dharma@gmail.com>
2022-04-28 20:39:20 +0200

    feat:preview build

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import LygtenGallery from "./components/Galleries/LygtenGallery";
 function App() {
   return (
   <div>
-      <HashRouter basename={`/${process.env.PUBLIC_URL}`}>
+      <HashRouter>
         <Routes>
-          <Route exact path={"/"} element={<ApplicationFrame></ApplicationFrame>}></Route>
-          <Route exact path={"/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
+          <Route exact path={"/preview"} element={<ApplicationFrame></ApplicationFrame>}></Route>
+          <Route exact path={"/preview/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
         </Routes>
       </HashRouter>
   </div>
