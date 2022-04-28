7d749d4dcc773ebe97e7d3ebdf6b90e4ad31c22e
Kasper Pedersen <kasper.dharma@gmail.com>
2022-04-28 20:35:24 +0200

    feat:preview build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom"
 
 import ApplicationFrame from "./components/Frame/ApplicationFrame";
 import LygtenGallery from "./components/Galleries/LygtenGallery";
@@ -8,12 +8,12 @@ import LygtenGallery from "./components/Galleries/LygtenGallery";
 function App() {
   return (
   <div>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+      <HashRouter basename={`/${process.env.PUBLIC_URL}`}>
         <Routes>
-          <Route exact path={""} element={<ApplicationFrame></ApplicationFrame>}></Route>
+          <Route exact path={"/"} element={<ApplicationFrame></ApplicationFrame>}></Route>
           <Route exact path={"/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
         </Routes>
-    </BrowserRouter>
+      </HashRouter>
   </div>
   );
 }
--- src/components/Frame/Header.jsx
+++ src/components/Frame/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
     return(
         <div className={"header-element"}>
-            <a href={""}><h1>UP AROUND THE BAND</h1></a>
+            <a href={"/"}><h1>UP AROUND THE BAND</h1></a>
             <p className={"subtitle"}>Creedence Clearwater Revival Cover Band</p>
             <video id={"background-video"} autoPlay={true} muted={true} loop={true}>
                 <source src="https://dl.dropboxusercontent.com/s/pyq9vlxgis84vuu/2022_21_04_LYGTENS_KRO.MP4?raw=1" type={"video/mp4"}/>
