3b3aee564711a742be1204ffb2ad79ee1d38b9e1
Kasper Pedersen <kasper.dharma@gmail.com>
2022-04-29 12:11:35 +0200

    refactor:changed to local routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import ApplicationFrame from "./components/Frame/ApplicationFrame";
 import LygtenGallery from "./components/Galleries/LygtenGallery";
@@ -9,12 +9,12 @@ function App() {
     console.log(process.env.PUBLIC_URL)
   return (
   <div>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path={"/"} element={<ApplicationFrame></ApplicationFrame>}></Route>
           <Route exact path={"/lygten-gallery"} element={<LygtenGallery></LygtenGallery>}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
   </div>
   );
 }
